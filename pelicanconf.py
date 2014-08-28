913e51d74d928e2ec5510a0dab6c68b801dfb6e2
Oscar Carlsson <ocarlsson3@gmail.com>
2014-08-28 18:15:56 +0200

    Fix feed

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,31 +4,34 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Oscar Carlsson'
 SITENAME = u'Oscar techblog'
-SITEURL = ''
-
+SITEURL = 'http://oscarlsson.se'
+# When testing locally, set ''
+# to get links right
+#SITEURL = ''
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 DISPLAY_PAGES_ON_MENU = "True"
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = "Test"
+FEED_RSS = "feeds/rss.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('github', 'http://www.github.com/oscarlsson'),
           ('linkedin', 'http://se.linkedin.com/pub/oscar-carlsson/63/3b7/801'),
           ('twitter', 'http://www.twitter.com/oscarlsson'),
           ('tumblr', 'http://oscarlsson.tumblr.com/'),
-          ('facebook', 'http://www.facebook.com/oscarlsson'))
-
+          ('facebook', 'http://www.facebook.com/oscarlsson'),
+          ('rss', SITEURL + '/feeds/rss.xml'))
+FAVICON = SITEURL + '/images/favicon.png'
 DEFAULT_PAGINATION = 10
 THEME = "../pelican-themes/SoMABlog"
 
@@ -37,7 +40,6 @@ PLUGINS = ['assets', 'sitemap', 'gravatar', "disqus_static"]
 
 DISQUS_SITENAME = u'oscarstechblog'
 
-#SITESUBTITLE = "SK"
 SITETAGLINE = "a developer's blog"
 DELETE_OUTPUT_DIRECTORY = False
 TWITTER_USERNAME = "oscarlsson"
